2011b5aea92e21b4ac7276ee887bb5eb972d835a
S <s>
2013-12-16 00:51:04 -0800

    {"type":"TI", "runs":"0"}

--- MidpointFindingKarel.java
+++ MidpointFindingKarel.java
@@ -74,7 +74,7 @@ public class MidpointFindingKarel extends SuperKarel {
 	// method to map the internal circumference -- second round
 	private void mapInternal() {
 		adjustToNorth();
-		while (true) {
+		while (true) { // M.H. modify this condition
 			moveToTrailingBeeper();
 			turnRight();
 		}
