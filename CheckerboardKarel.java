ef0decd4f790a3da78cdae34d0ce0f2498fe0239
S <s>
2013-12-15 22:15:14 -0800

    {"type":"TI", "runs":"0"}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -12,16 +12,24 @@ public class CheckerboardKarel extends SuperKarel {
 	
 	public void run() {
 		
+		// draw the first line
+		drawRow();         // draw check pattern in a row
+		adjustNorth();
 		while(frontIsClear()) { // check if the the front of Karel is clear
+			move();
 			drawRow();         // draw check pattern in a row
 			adjustNorth();
-			
 		}
 	}
 	
 	// method to draw a check pattern in a row
 	private void drawRow () {
-		
+		while(frontIsClear()) {
+			putBeeper();
+			
+			move();
+			move();
+		}
 	}
 	
 	// method to adjust Karel's direction to face North
