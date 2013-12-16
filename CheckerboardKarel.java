774412d13559c6c871659031338afe635f769f1b
S <s>
2013-12-15 22:10:17 -0800

    {"type":"TI", "runs":"3"}

--- CheckerboardKarel.java
+++ CheckerboardKarel.java
@@ -1,16 +1,42 @@
 /*
  * File: CheckerboardKarel.java
  * ----------------------------
- * When you finish writing it, the CheckerboardKarel class should draw
- * a checkerboard using beepers, as described in Assignment 1.  You
- * should make sure that your program works for all of the sample
- * worlds supplied in the starter folder.
+ * Karel will draw a checkerboard using beepers. 
+ * Karel will instantiate SuperKarel Class, additional turnRight() and turnAround()
+ * method are available.
  */
 
 import stanford.karel.*;
 
 public class CheckerboardKarel extends SuperKarel {
-
-	// You fill in this part
-
+	
+	public void run() {
+		
+		while(frontIsClear()) { // check if the the front of Karel is clear
+			drawRow();         // draw check pattern in a row
+			adjustNorth();
+			
+		}
+	}
+	
+	// method to draw a check pattern in a row
+	private void drawRow () {
+		
+	}
+	
+	// method to adjust Karel's direction to face North
+	// pre-condition : Karel faces any direction
+	// post-condition: Karel is facing North
+	private void adjustNorth(){
+		if (facingEast()) {
+			turnLeft();
+		}
+		if (facingWest()) {
+			turnRight();
+		}
+		if(facingSouth()) {
+			turnAround();
+		}
+	}
+	
 }
